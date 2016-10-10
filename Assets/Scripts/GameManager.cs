48daa070fbc7994c33458b9dbdb25dd29358de0c
camilohoyos16 <acamilohoyos@hotmail.com>
2016-10-10 16:29:14 -0500

    Chocita :house:
    Se agraga la choza y se arregla el problema del script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -29,7 +29,7 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		
-		KidsCollision.kidOnHouse += MoreScore ();
+		KidsCollision.kidOnHouse += MoreScore;
 		
 	}
 	
